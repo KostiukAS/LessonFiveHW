242ef905d51e0ceff2490607cd688608a61498ab
KostiukAS <kostiukandrey2005@gmail.com>
2021-04-12 22:57:39 +0300

    Updated storyboard.

---
+++ LessonFiveHW/ViewControllers/LoginScreenController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  LessonFiveHW
+//
+//  Created by admin on 12.04.2021.
+//
+
+import UIKit
+
+class LoginScreenController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var loginButton: UIButton!
+    
+    @IBOutlet var nameRecoveryButton: UIButton!
+    @IBOutlet var passwordRecoveryButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LessonFiveHW/ViewControllers/MainScreenController.swift
@@ -0,0 +1,34 @@
+//
+//  MainScreenController.swift
+//  LessonFiveHW
+//
+//  Created by admin on 12.04.2021.
+//
+
+import UIKit
+
+class MainScreenController: UIViewController {
+    
+    @IBOutlet var mainScreenNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mainScreenNameLabel.text = ""
+        
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
