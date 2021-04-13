cf51d2be6e63ea54ff4fca2c7fcfc196bdd90578
KostiukAS <kostiukandrey2005@gmail.com>
2021-04-13 07:47:02 +0300

    New recovery

---
+++ LessonFiveHW/ViewControllers/InstructionScreenController.swift
@@ -0,0 +1,31 @@
+//
+//  InstructionScreenController.swift
+//  LessonFiveHW
+//
+//  Created by admin on 13.04.2021.
+//
+
+import UIKit
+
+class InstructionScreenController: UIViewController {
+    
+    @IBOutlet var recoveryExitButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func unwindSegue(_ sender: UIStoryboardSegue) {}
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
